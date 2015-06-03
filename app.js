fbebdf4dc5dfb7636eafbac491bef4a2d6b40aab
Pius Nyakoojo <pnyakoojo.16@gmail.com>
2015-06-02 23:40:11 -0500

    updated server ip and port listener

--- app.js
+++ app.js
@@ -39,6 +39,13 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-    console.log('listening on *: 3000');
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+http.listen(port, ip_address, function(){
+    console.log( "Listening on " + ip_address + ", server_port " + port );
 });
+
+//http.listen(3000, function(){
+//   console.log('listening on *: 3000');
+//});
