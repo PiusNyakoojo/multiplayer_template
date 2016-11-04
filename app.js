6d77b0436e92253d93ca6ddcd97b3feaa8ea7130
Pius Nyakoojo <pnyakoojo.16@gmail.com>
2016-11-03 23:53:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ io.on('connection', function(socket){
 
 });
 
+// Handle environment changes
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
